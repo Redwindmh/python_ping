3ea7af56bf49e8377c7be5e1d3b74516afd5ab0c
Malcolm Hendricks <malcolm@rn-c.co.jp>
2022-02-03 19:13:03 +0900

    Adding clocks

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 import pygame, sys, random
 
-
+# functions
 def ball_animation():
-    global ball_speed_x, ball_speed_y
+    global ball_speed_x, ball_speed_y, player_score, opponent_score, score_time
     ball.x += ball_speed_x
     ball.y += ball_speed_y
 
     if ball.top <= 0 or ball.bottom >= screen_height:
         ball_speed_y *= -1
-    if ball.left <= 0 or ball.right >= screen_width:
-        ball_recenter()
+
+    if ball.left <= 0:
+        player_score += 1
+        score_time = pygame.time.get_ticks()
+
+    if ball.right >= screen_width:
+        opponent_score += 1
+        score_time = pygame.time.get_ticks()
 
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_speed_x *= -1
@@ -36,30 +42,47 @@ def opponent_animation():
 
 def ball_recenter():
     global ball_speed_x, ball_speed_y
+    current_time = pygame.time.get_ticks()
     ball.center = (screen_width / 2, screen_height / 2)
+    if current_time - score_time < 2100:
+        ball_speed_x, ball_speed_y = 0
     ball_speed_y *= random.choice((1, -1))
     ball_speed_x *= random.choice((1, -1))
 
 
+# Game initialization
 pygame.init()
 clock = pygame.time.Clock()
 
+# Screen settings
 screen_width = 1280
 screen_height = 960
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Ping")
 
+# Game rectangles
 ball = pygame.Rect(screen_width / 2 - 15, screen_height / 2 - 15, 30, 30)
 player = pygame.Rect(screen_width - 20, screen_height / 2 - 70, 10, 140)
 opponent = pygame.Rect(10, screen_height / 2 - 70, 10, 140)
 
+# Colors
 bg_color = pygame.Color("grey12")
 light_grey = (200, 200, 200)
 
+# Game variables
 ball_speed_x = 7 * random.choice((1, -1))
 ball_speed_y = 7 * random.choice((1, -1))
 player_speed = 0
 opponent_speed = 7
+
+# Text variables
+player_score = 0
+opponent_score = 0
+game_font = pygame.font.Font("freesansbold.ttf", 34)
+
+# Score timer
+score_time = None
+game_time = pygame.time.get_ticks
 
 while True:
     for event in pygame.event.get():
@@ -77,10 +100,12 @@ while True:
             if event.key == pygame.K_UP:
                 player_speed += 7
 
+    # Game logic
     ball_animation()
     player_animation()
     opponent_animation()
 
+    # Visuals
     screen.fill(bg_color)
     pygame.draw.rect(screen, light_grey, player)
     pygame.draw.rect(screen, light_grey, opponent)
@@ -91,6 +116,19 @@ while True:
         (screen_width / 2, 0),
         (screen_width / 2, screen_height),
     )
+
+    if score_time:
+        ball_recenter()
+
+    player_text = game_font.render(f"{player_score}", False, light_grey)
+    screen.blit(player_text, (660, 470))
+
+    opponent_text = game_font.render(f"{opponent_score}", False, light_grey)
+    screen.blit(opponent_text, (600, 470))
+
+    # Top timer
+    top_timer = game_font.render(f"{game_time}", False, light_grey)
+    screen.blit(top_timer, (500, 500))
 
     pygame.display.flip()
     clock.tick(60)
